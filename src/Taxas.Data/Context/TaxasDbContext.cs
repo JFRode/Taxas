aac6e01dd40f7b6edeaef1a422d146f298222abd
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 10:35:16 -0300

    Adicionado DbContext

---
+++ src/Taxas.Data/Context/TaxasDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Taxas.Domain.TaxaDeJuros;
+
+namespace Taxas.Data.Context
+{
+    public class TaxasDbContext : DbContext
+    {
+        public DbSet<TaxaDeJuros> TaxasDeJuros { get; set; }
+
+        public TaxasDbContext(DbContextOptions<TaxasDbContext> options) : base(options)
+        {
+        }
+    }
+}
