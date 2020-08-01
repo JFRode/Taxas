bccfc7fafd926c3ff0c2279e66953c82e3789259
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 10:53:13 -0300

    Adicionado TaxaDeJurosService

---
+++ src/Taxas.Application/Services/TaxaDeJurosService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using SDK.Dtos;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Taxas.Application.Interfaces;
+using Taxas.Application.Interfaces.Services;
+
+namespace Taxas.Application.Services
+{
+    public class TaxaDeJurosService : ITaxaDeJurosService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public TaxaDeJurosService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<TaxaDeJurosDto> GetAsync(CancellationToken cancellationToken)
+        {
+            var taxaDeJuros = await Task.Run(() => _unitOfWork.TaxaDeJurosRepository.SelectAll().FirstOrDefault(), cancellationToken);
+            return _mapper.Map<TaxaDeJurosDto>(taxaDeJuros);
+        }
+    }
+}
