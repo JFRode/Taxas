f82915d563636e59a458a3200576fc7e9748663b
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 10:35:42 -0300

    Adicionado TaxaDeJurosRepository

---
+++ src/Taxas.Application/Interfaces/Repositories/ITaxaDeJurosRepository.cs
@@ -0,0 +1,9 @@
+﻿using SDK.Base.Interfaces;
+using Taxas.Domain.TaxaDeJuros;
+
+namespace Taxas.Application.Interfaces.Repositories
+{
+    public interface ITaxaDeJurosRepository : IRepository<TaxaDeJuros>
+    {
+    }
+}
---
+++ src/Taxas.Data/Repositories/TaxaDeJurosRepository.cs
@@ -0,0 +1,14 @@
+﻿using SDK.Base;
+using Taxas.Application.Interfaces.Repositories;
+using Taxas.Data.Context;
+using Taxas.Domain.TaxaDeJuros;
+
+namespace Taxas.Data.Repositories
+{
+    public class TaxaDeJurosRepository : BaseRepository<TaxaDeJuros>, ITaxaDeJurosRepository
+    {
+        public TaxaDeJurosRepository(TaxasDbContext taxasDbContext) : base(taxasDbContext)
+        {
+        }
+    }
+}
