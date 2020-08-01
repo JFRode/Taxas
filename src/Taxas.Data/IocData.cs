7c53af09006527651d5f4920dda678254dcd31fb
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 10:39:40 -0300

    Adicionado IocData

---
+++ src/Taxas.Data/IocData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Taxas.Application.Interfaces;
+using Taxas.Application.Interfaces.Repositories;
+using Taxas.Data.Context;
+using Taxas.Data.Repositories;
+using Taxas.Data.Seed;
+
+namespace Taxas.Data
+{
+    public static class IocData
+    {
+        public static IServiceCollection AddDataServices(this IServiceCollection services) =>
+            services.AddScoped<ITaxaDeJurosSeed, TaxaDeJurosSeed>()
+                    .AddScoped<ITaxaDeJurosRepository, TaxaDeJurosRepository>()
+                    .AddScoped<IUnitOfWork, UnitOfWork>();
+
+        public static IServiceCollection AddTaxasDbContext(this IServiceCollection services) =>
+            services.AddDbContext<TaxasDbContext>(opt => opt.UseInMemoryDatabase("Taxas"));
+    }
+}
