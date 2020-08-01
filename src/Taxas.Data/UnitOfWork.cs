54f57f67718532b7454c94fea17990a92a188ac3
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 10:39:17 -0300

    Adicionado UnitOfWork

---
+++ src/Taxas.Data/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Taxas.Application.Interfaces;
+using Taxas.Application.Interfaces.Repositories;
+using Taxas.Data.Context;
+using Taxas.Data.Repositories;
+
+namespace Taxas.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TaxasDbContext _taxasDbContext;
+        private ITaxaDeJurosRepository _taxaDeJurosRepository;
+        private bool _disposed;
+
+        public UnitOfWork(TaxasDbContext taxasDbContext)
+        {
+            _taxasDbContext = taxasDbContext;
+        }
+
+        public ITaxaDeJurosRepository TaxaDeJurosRepository => _taxaDeJurosRepository ??= new TaxaDeJurosRepository(_taxasDbContext);
+
+        public void SaveChanges()
+        {
+            _taxasDbContext.SaveChanges();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            return _taxasDbContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing)
+            {
+                _taxasDbContext?.Dispose();
+                _taxaDeJurosRepository?.Dispose();
+            }
+
+            _disposed = true;
+        }
+    }
+}
