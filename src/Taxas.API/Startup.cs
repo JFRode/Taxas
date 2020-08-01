d7d275aa6037e77fe89f53e60a646328ba7b99c4
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 11:41:55 -0300

    Configurado Swagger

--- src/Taxas.API/Startup.cs
+++ src/Taxas.API/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using Taxas.Data;
 using Taxas.Data.Seed;
 using Taxas.Application;
+using Microsoft.OpenApi.Models;
 
 namespace Taxas.API
 {
@@ -36,6 +37,8 @@ namespace Taxas.API
 
             services.AddDataServices();
             services.AddTaxasDbContext();
+
+            ConfigureSwaggerService(services);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -53,6 +56,12 @@ namespace Taxas.API
 
             SeedDataBase(app);
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Taxas.API v1");
+            });
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
@@ -64,6 +73,31 @@ namespace Taxas.API
             var serviceScopeFactory = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
             var seed = serviceScopeFactory.ServiceProvider.GetService<ITaxaDeJurosSeed>();
             seed.Executar();
+        }
+
+        private void ConfigureSwaggerService(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Taxas.API",
+                    Version = "v1",
+                    Description = "API de consulta de taxas para cálculo",
+                    TermsOfService = new Uri("https://github.com/JFRode/Taxas/blob/master/LICENSE"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "João Felipe Gonçalves",
+                        Email = "joaofelipe.rode@gmail.com",
+                        Url = new Uri("https://github.com/JFRode"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under MIT",
+                        Url = new Uri("https://github.com/JFRode/Taxas/blob/master/LICENSE"),
+                    }
+                });
+            });
         }
     }
 }
