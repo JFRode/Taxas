b822a67828895035996f7094fbec88a03773e813
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 10:48:25 -0300

    Configurado profile do AutoMapper

---
+++ src/Taxas.Application/Mappers/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SDK.Dtos;
+using Taxas.Domain.TaxaDeJuros;
+
+namespace Taxas.Application.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<TaxaDeJuros, TaxaDeJurosDto>().ReverseMap();
+        }
+    }
+}
