4a623349aa284eb751135e9e1305ef3d7d6233d1
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 11:37:36 -0300

    Adicionado TaxaDeJurosController

---
+++ src/Taxas.API/Controllers/TaxaDeJurosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Taxas.Application.Interfaces.Services;
+
+namespace Taxas.API.Controllers
+{
+    [Route("taxaJuros")]
+    public class TaxaDeJurosController : Controller
+    {
+        private readonly ITaxaDeJurosService _taxaDeJurosService;
+
+        public TaxaDeJurosController(ITaxaDeJurosService taxaDeJurosService)
+        {
+            _taxaDeJurosService = taxaDeJurosService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var taxaDeJuros = await _taxaDeJurosService.GetAsync(cancellationToken);
+                return Ok(taxaDeJuros.Percentual);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.StackTrace);
+            }
+        }
+    }
+}
