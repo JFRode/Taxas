64e1cefc82b6cc8c175a9b42ca268a0b30073af9
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-01 10:49:54 -0300

    Adicionado TaxaDeJurosValidator

---
+++ src/Taxas.Application/Validators/TaxaDeJurosValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using SDK.Dtos;
+using System;
+using Taxas.Application.Interfaces.Validators;
+
+namespace Taxas.Application.Validators
+{
+    public class TaxaDeJurosValidator : AbstractValidator<TaxaDeJurosDto>, ITaxaDeJurosValidator
+    {
+        public TaxaDeJurosValidator()
+        {
+            RuleFor(x => x)
+                .NotNull().WithMessage("O objeto TaxaDeJuros está nulo.");
+
+            RuleFor(x => x.Id)
+                .NotEqual(Guid.Empty)
+                .WithMessage("O campo Id do objeto TaxaDeJuros está vazio.");
+        }
+    }
+}
