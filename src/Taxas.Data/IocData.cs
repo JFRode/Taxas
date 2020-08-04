55721ba9ec259421ac040ecfdcdfe9f0f3a2a866
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-08-04 14:56:31 -0300

    Alterado InMemory para SQLServer

--- src/Taxas.Data/IocData.cs
+++ src/Taxas.Data/IocData.cs
@@ -4,18 +4,16 @@ using Taxas.Application.Interfaces;
 using Taxas.Application.Interfaces.Repositories;
 using Taxas.Data.Context;
 using Taxas.Data.Repositories;
-using Taxas.Data.Seed;
 
 namespace Taxas.Data
 {
     public static class IocData
     {
         public static IServiceCollection AddDataServices(this IServiceCollection services) =>
-            services.AddScoped<ITaxaDeJurosSeed, TaxaDeJurosSeed>()
-                    .AddScoped<ITaxaDeJurosRepository, TaxaDeJurosRepository>()
+            services.AddScoped<ITaxaDeJurosRepository, TaxaDeJurosRepository>()
                     .AddScoped<IUnitOfWork, UnitOfWork>();
 
-        public static IServiceCollection AddTaxasDbContext(this IServiceCollection services) =>
-            services.AddDbContext<TaxasDbContext>(opt => opt.UseInMemoryDatabase("Taxas"));
+        public static IServiceCollection AddTaxasDbContext(this IServiceCollection services, string connectionString) =>
+            services.AddDbContext<TaxasDbContext>(opt => opt.UseSqlServer(connectionString));
     }
 }
